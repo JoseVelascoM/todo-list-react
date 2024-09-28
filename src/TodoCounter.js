7ba0b23ff0ef024970b412a329269e17c1ef1dd7
Jose Velasco <josepe_789@hotmail.com>
2024-09-27 21:41:43 -0400

    Display a message when all the todo items are completed

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -3,7 +3,16 @@ import './TodoCounter.css';
 function TodoCounter({ total, completed }) {
   return (
     <h1 className='todo-counter'>
-      Has completado <strong>{completed}</strong> de <strong>{total}</strong> TODOS
+      { total === completed ? (
+        <strong>
+          ¡Felicidades has completado todas tus tareas!
+        </strong>
+      ) : (
+        <>
+          Has completado <strong>{completed}</strong> de <strong>{total}</strong> TODOS
+        </>
+      )
+      }
     </h1>
   );
 }
