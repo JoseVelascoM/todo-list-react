3f2fbad498089e012508b21fb9277a381356f9a2
Jose Velasco <josepe_789@hotmail.com>
2024-09-27 21:36:18 -0400

    Allow user to complete and delete a todo item

--- src/App.js
+++ src/App.js
@@ -23,6 +23,22 @@ function App() {
   const totalTodos = todos.length;
   const searchedTodos = todos.filter(todo => todo.text.toLowerCase().includes(searchValue.toLowerCase()));
 
+  const handleCompleteTodo = (completedTodo) => {
+    const newTodos = [...todos];
+    const index = newTodos.findIndex(todo => todo.text === completedTodo.text);
+    newTodos[index].completed = !newTodos[index].completed;
+
+    setTodos(newTodos);
+  }
+
+  const handleDeleteTodo = (deletedTodo) => {
+    const newTodos = [...todos];
+    const index = newTodos.findIndex(todo => todo.text === deletedTodo.text);
+    newTodos.splice(index, 1);
+
+    setTodos(newTodos);
+  }
+
   return (
     <React.Fragment>
       <TodoCounter completed={completedTodos} total={totalTodos} />
@@ -30,7 +46,13 @@ function App() {
 
       <TodoList>
         {searchedTodos.map((todo, index) => (
-          <TodoItem completed={todo.completed} key={index} text={todo.text} />
+          <TodoItem
+            completed={todo.completed}
+            key={index}
+            text={todo.text}
+            onComplete={() => handleCompleteTodo(todo)}
+            onDelete={() => handleDeleteTodo(todo)}
+          />
         ))}
       </TodoList>
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,11 @@
 import './TodoItem.css';
 
-function TodoItem({ text, completed }) {
+function TodoItem({ text, completed, onComplete, onDelete }) {
   return (
     <li className='todo-item'>
-      <span className={`icon icon-check ${completed ? 'icon-check--active' : ''}`}>V</span>
+      <span className={`icon icon-check ${completed ? 'icon-check--active' : ''}`} onClick={onComplete}>V</span>
       <p className={`todo-item-p ${completed ? 'todo-item-p--complete' : ''}`}>{text}</p>
-      <span className='icon icon-delete'>X</span>
+      <span className='icon icon-delete' onClick={onDelete}>X</span>
     </li>
   );
 }
